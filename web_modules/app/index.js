eea0e164f4225a0cab7eb51c2d87b56e69499c7d
David Lormor <davidlormor@gmail.com>
2016-05-29 15:12:19 -0500

    Lesson 14

--- web_modules/app/index.js
+++ web_modules/app/index.js
@@ -1,78 +1,30 @@
-import React, { Component, PropTypes } from 'react'
-import { findDOMNode } from 'react-dom'
+import React, { Component } from 'react'
 
 class App extends Component {
   constructor () {
     super()
-    this.update = this.update.bind(this)
     this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
+      people: [
+        {id: 1, name: 'David'},
+        {id: 2, name: 'Jordan'},
+        {id: 3, name: 'Renick'},
+        {id: 4, name: 'Galilee'},
+        {id: 5, name: 'Magdalene'}
+      ]
     }
   }
 
-  update () {
-    const { red, green, blue } = this.refs
-
-    this.setState({
-      red: findDOMNode(red.refs.inp.refs.inp).value,
-      green: findDOMNode(green.refs.inp.refs.inp).value,
-      blue: findDOMNode(blue.refs.inp.refs.inp).value
+  render () {
+    const people = this.state.people.map((person) => {
+      return <Person key={person.id} person={person} />
     })
-  }
 
-  render () {
-    const { red, green, blue } = this.state
-
-    return (
-      <div>
-        <ColorSlider ref='red' label='R' value={+red} update={this.update} /> - {red}
-        <ColorSlider ref='green' label='G' value={+green} update={this.update} /> - {green}
-        <ColorSlider ref='blue' label='B' value={+blue} update={this.update} /> - {blue}
-      </div>
-    )
+    return <div>{people}</div>
   }
 }
 
-class ColorSlider extends Component {
-  render () {
-    return <NumInput ref='inp' type='number' min={0} max={255} step={1} {...this.props} />
-  }
-}
-
-class NumInput extends Component {
-  render () {
-    const {
-      min,
-      max,
-      step,
-      value,
-      label,
-      update,
-      type
-    } = this.props
-
-    return (
-      <div>
-        <label>{label}</label>
-        <input ref='inp' type={type} min={min} max={max} step={step} value={value} onChange={update} />
-      </div>
-    )
-  }
-}
-
-NumInput.propTypes = {
-  min: PropTypes.number,
-  max: PropTypes.number,
-  step: PropTypes.number,
-  value: PropTypes.number,
-  label: PropTypes.string,
-  update: PropTypes.func.isRequired,
-  type: PropTypes.oneOf(['number', 'range'])
-}
-
-NumInput.defaultProps = {
+const Person = ({ person: { id, name } }) => {
+  return <div>{id}. {name}</div>
 }
 
 export default App
