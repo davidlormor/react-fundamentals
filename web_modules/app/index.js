e34b052635c6a2e5dee7ddda3d02ed17055235a8
David Lormor <davidlormor@gmail.com>
2016-05-29 12:08:44 -0500

    Lesson 5 cleanup

--- web_modules/app/index.js
+++ web_modules/app/index.js
@@ -1,44 +1,26 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 class App extends Component {
   constructor () {
     super()
-    this.state = {
-      txt: 'the number is: ',
-      cat: 0
-    }
+    this.state = { txt: 'txt' }
   }
 
-  updateTxt (e) {
+  update (e) {
     this.setState({ txt: e.target.value })
   }
 
-  updateCat (e) {
-    this.setState({ cat: e.target.value })
-  }
-
   render () {
-    const { txt, cat } = this.state
+    const { txt } = this.state
 
     return (
       <div>
-        <input type='text' onChange={this.updateTxt.bind(this)} value={txt} />
-        <br />
-        <input type='number' onChange={this.updateCat.bind(this)} value={cat} />
-        <h1>{txt} {cat}</h1>
+        <input type='text' value={txt} onChange={this.update.bind(this)} />
+        <h1>{txt}</h1>
       </div>
     )
   }
-}
-
-App.propTypes = {
-  txt: PropTypes.string.isRequired,
-  cat: PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'Welcome, user #'
 }
 
 render(
