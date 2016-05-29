f99b06a4aed250ce9d953c8278f44d70675e99d9
David Lormor <davidlormor@gmail.com>
2016-05-29 13:44:30 -0500

    Lesson 9

--- web_modules/app/index.js
+++ web_modules/app/index.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react'
-import { render } from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = { val: 0 }
+    this.update = this.update.bind(this)
+  }
+
+  componentWillMount () {
+    console.log('mounting')
+  }
+
+  componentDidMount () {
+    console.log('mounted')
+  }
+
+  componentWillUnmount () {
+    console.log('unmounting')
+  }
+
+  update () {
+    console.log('updating')
+
+    this.setState({ val: this.state.val + 1 })
+  }
+
   render () {
-    return <Button>Click Me!</Button>
+    console.log('rendering')
+
+    return <button onClick={this.update}>{this.state.val}</button>
   }
 }
 
-class Button extends Component {
+class Wrapper extends Component {
+  constructor () {
+    super()
+    this.mount = this.mount.bind(this)
+    this.unmount = this.unmount.bind(this)
+  }
+  mount () {
+    render(<App />, document.getElementById('a'))
+  }
+
+  unmount () {
+    unmountComponentAtNode(document.getElementById('a'))
+  }
+
   render () {
-    return <button>{this.props.children}</button>
+    return (
+      <div>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
+        <div id='a'></div>
+      </div>
+    )
   }
 }
 
 render(
-  <App />,
+  <Wrapper />,
   document.getElementById('app')
 )
