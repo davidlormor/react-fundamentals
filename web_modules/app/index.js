69f527c4bd241cf5900bbe90d57f5677cbf2cc66
David Lormor <davidlormor@gmail.com>
2016-05-29 13:31:54 -0500

    Lesson 8

--- web_modules/app/index.js
+++ web_modules/app/index.js
@@ -1,55 +1,19 @@
 import React, { Component } from 'react'
-import { findDOMNode, render } from 'react-dom'
+import { render } from 'react-dom'
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-    this.update = this.update.bind(this)
-  }
-
-  update (e) {
-    const { red, green, blue } = this.refs
-
-    this.setState({
-      red: findDOMNode(red).value,
-      green: findDOMNode(green).value,
-      blue: findDOMNode(blue).value
-    })
-  }
-
   render () {
-    const { red, green, blue } = this.state
-
-    return (
-      <div>
-        <h1>Red</h1>
-        <Slider ref='red' value={red} update={this.update} />
-        {red}
-        <h1>Green</h1>
-        <Slider ref='green' value={green} update={this.update} />
-        {green}
-        <h1>Blue</h1>
-        <Slider ref='blue' value={blue} update={this.update} />
-        {blue}
-      </div>
-    )
+    return <Button>Click Me!</Button>
   }
 }
 
-class Slider extends Component {
+class Button extends Component {
   render () {
-    const { update, value } = this.props
-
-    return <input type='range' min={0} max={255} value={value} onChange={update} />
+    return <button>{this.props.children}</button>
   }
 }
 
 render(
-  <App cat={5} />,
+  <App />,
   document.getElementById('app')
 )
